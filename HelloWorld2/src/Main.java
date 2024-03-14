5bf5da7c7f0866b5aa84c557026a4a697fbb6a5a
kristina.demchenko <k.demchenko20203108@gmail.com>
2024-03-14 15:05:28 +0300

    2 версия домашки (черновик)

--- HelloWorld2/src/Main.java
+++ HelloWorld2/src/Main.java
@@ -4,27 +4,49 @@ public class Main {
     public static void main(String[] args) {
 
         int score = new Scanner(System.in).nextInt();
+        System.out.print("Please enter your score");
+        //int score = Scanner .nextLine();
 
-        while (score > 100) {
-            System.out.print("Please enter your score");
-            int score = scanner.nextLine();
-            for (score > 100) ; {
-                System.out.println("Try again! Please enter your score:");
+        if ((score > 100 ) || (score < 0)) {
+
+            do {
+                System.out.print("Try again! Please enter your score");
+                score = new Scanner(System.in).nextInt();
+            }
+            while ((score > 100) ||(score < 0));
+            /* for (score > 100) ; {
+                System.out.println(" Please enter your score:");
                 int score = scanner.nextLine();
                 System.out.print("Please enter your score");
-            }
-        }
-                if (score >= 70) {
-                    System.out.println("Congrats! You’ve passed the test!");
-                } else {
-                    System.out.println("Sorry, you’ve failed the test.");
-                }
+            } */
+        } else if (score >= 70) {
+                System.out.println("Congrats! You’ve passed the test!");
+            } else System.out.println("Sorry, you’ve failed the test.");
 
-            }
         }
     }
 
+// задание 2.
+
+//        String str = new String();
+//        Scanner scanner = new Scanner(System.in);
+//        while (str.length() == 0) {
+//            System.out.print("Please enter something:");
+//            str = scanner.nextLine();
+//        }
+
 //        Задание 3.
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Please enter a number: ");
+//        int x = scanner.nextInt();
+//        int sum = 0;
+//        for (int i = 0; i <= x; i++) {
+//            sum += i;
+//        }
+//        System.out.printf("The sum of all numbers from 0 to %d is %d", x, sum);
+
+
+//        Задание 4.
 
 //        System.out.println("Choose your channel: \n1 - NTV \n2 - Russia\n3 - RenTV\n4- Culture\n5 - Fox3");
 //        int channels = new Scanner(System.in).nextInt();
